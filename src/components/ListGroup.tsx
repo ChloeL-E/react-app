db65ade89857374245106bb7a39fb806c1bf658d
ChloeL-E <chloe_c123@hotmail.co.uk>
2025-01-22 21:12:02 +0000

    Refactored code to dynamially render datat

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,14 @@
 function ListGroup() {
+  const items = ["Newcastle", "Sunderland", "Hexham", "Gateshead", "Morpeth"];
+  // no for loops allowed in jsx so to loop through a list and display the items dynamically
+  // we can us the map() function to convert each item in the items array to a list item and use and arrow function + jsx {} to display each item.
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
