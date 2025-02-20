49bd22cec100e8ba1a59ab6ddf90bb21e4b0f9e6
ChloeL-E <chloe_c123@hotmail.co.uk>
2025-02-20 11:24:46 +0000

    Applied form validation using zod

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,108 @@
-import React, { FormEvent, useRef } from "react";
+import React, { FormEvent, useRef, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be at least 3 characters" }),
+  age: z
+    .number({ invalid_type_error: "Age field is required" })
+    .min(18, { message: "Age must be at least 18" }),
+});
+
+type FormData = z.infer<typeof schema>;
+
+/* Managing forms with React Hook Form */
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const onSubmit = (data: FieldValues) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          {...register("name")}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+        {errors.name && <p className="text-danger">{errors.name.message}</p>}
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="for-label">
+          Age
+        </label>
+        <input
+          {...register("age", { valueAsNumber: true })}
+          id="age"
+          type="number"
+          className="form-control"
+        />
+        {errors.age && <p className="text-danger">{errors.age.message}</p>}
+      </div>
+      <button disabled={!isValid} type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+/*const [person, setPerson] = useState({       ** Using useState to get dtat from inout fields 
+    name: "",
+    age: "",
+  });
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(person);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          value={person.name}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="for-label">
+          Age
+        </label>
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, age: parseInt(event.target.value) })
+          }
+          value="person.age"
+          id="age"
+          type="number"
+          className="form-control"
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};*/
+
+/*const nameRef = useRef<HTMLInputElement>(null);    ** using Ref Hook to send person object to console from form input data
   const ageRef = useRef<HTMLInputElement>(null);
   const person = { name: "", age: 0 };
 
@@ -11,6 +112,7 @@ const Form = () => {
     if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
     console.log(person);
   };
+
   return (
     <form onSubmit={handleSubmit}>
       <div className="mb-3">
@@ -30,6 +132,6 @@ const Form = () => {
       </button>
     </form>
   );
-};
+};*/
 
 export default Form;
